3c8cdb42455ec9b1363354ea6884de40ce7ae168
ryan-c-lewis <ryan.cp.lewis@gmail.com>
2018-07-08 15:20:05 -0600

    Output ShapeNoteSong to MusicXML

--- src/shapenotesong.py
+++ src/shapenotesong.py
@@ -1,16 +1,23 @@
 import logging
 import music21
+from music21.musicxml import m21ToXml
 
 
 class ShapeNoteSong:
     @classmethod
     def from_file_path(cls, file_path):
-        logging.info("Loading file: " + file_path)
+        logging.debug("Loading file: " + file_path)
         score = music21.converter.parse(file_path)
         return cls(score)
 
+    @classmethod
+    def from_xml(cls, xml):
+        logging.debug("Loading xml: " + xml)
+        score = music21.converter.parseData(xml)
+        return cls(score)
+
     def __init__(self, score):
-        logging.debug("Creating ShpeNoteSong from music21 score: " + str(score))
+        logging.debug("Creating ShapeNoteSong from music21 score: " + str(score))
         self.score = score
         self.raw_title = score.metadata.title
         self.composer = score.metadata.composer
@@ -21,5 +28,13 @@ class ShapeNoteSong:
         # Assuming title format is "SONG NAME, PAGE #"
         self.song_name = self.raw_title[:self.raw_title.rfind(',')].strip()
         self.page_number = self.raw_title[self.raw_title.rfind(',')+1:].strip()
-        if sum(c.isdigit() for c in self.page_number) == 2: # If page # is between 10 and 99
+        # If page # is between 10 and 99, add a leading '0' so it's 3 digits
+        if sum(c.isdigit() for c in self.page_number) == 2:
             self.page_number = '0' + self.page_number
+
+    def to_score(self):
+        # TEMPORARY. This only works as long as we don't change the ShapeNoteSong at all.
+        return self.score
+
+    def to_musicxml(self):
+        return m21ToXml.GeneralObjectExporter(self.to_score()).parse().decode('utf-8')
--- test/test_shapenotesong.py
+++ test/test_shapenotesong.py
@@ -4,12 +4,21 @@ from src.shapenotesong import ShapeNoteSong
 
 
 class TestShapeNoteSong(unittest.TestCase):
+    def assert_47b_loaded_correctly(self, song):
+        self.assertEqual(music21.key.Key('A', 'minor'), song.key)
+        self.assertEqual(music21.pitch.Pitch('A2'), song.range.noteStart)
+        self.assertEqual(music21.pitch.Pitch('G5'), song.range.noteEnd)
+        self.assertEqual(4, len(song.parts))
+
     def test_load_music_info(self):
         result = ShapeNoteSong.from_file_path("..\\lib\\books\\denson\\47b.xml")
-        self.assertEqual(music21.key.Key('A', 'minor'), result.key)
-        self.assertEqual(music21.pitch.Pitch('A2'), result.range.noteStart)
-        self.assertEqual(music21.pitch.Pitch('G5'), result.range.noteEnd)
-        self.assertEqual(4, len(result.parts))
+        self.assert_47b_loaded_correctly(result)
+
+    def test_output_to_xml(self):
+        out_to_xml = ShapeNoteSong.from_file_path("..\\lib\\books\\denson\\47b.xml").to_musicxml()
+        # I can't think of a good way to test the output except to make sure we can read it back in correctly...
+        from_xml = ShapeNoteSong.from_xml(out_to_xml)
+        self.assert_47b_loaded_correctly(from_xml)
 
     def test_load_metadata(self):
         result = ShapeNoteSong.from_file_path("..\\lib\\books\\denson\\167.xml")
